fe0e80e40344f83b3c15cc0937c9ccf644c9d379
gostdarkMohAhmad <44299639+gostdarkMohAhmad@users.noreply.github.com>
2018-11-08 20:42:12 +0200

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("456134809484263447")
+setInterval(function() {
+channel.send(`***الفلوووس تغير النفووس***`);
+}, 25)
+})
+ 
+ 
+client.login('NDgzMzYwMDQ3NTQ4NzI3Mjk2.DsYBuA.xY8eEx4Y-Cwrfv4W-z-EhaH77C8');
